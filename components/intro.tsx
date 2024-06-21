f35be1d136d7902d89026a8d67989e9e5d9f708d
Toufiq <toufiq@Toufiqs-MacBook-Air.local>
2024-06-21 21:55:29 +0600

    Fix experience

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,7 +65,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Toufiqur Rahman.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">2 years</span> of experience. I enjoy
+        <span className="font-bold">almost 3 years</span> of experience. I enjoy
         building <span className="italic">cutting-edge solutions</span>. I am
         framework agnostic <span className="underline">Software Engineer</span>.
       </motion.h1>
